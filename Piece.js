9722e5798668fa38e5f894429d18bb6ecdf0021b
MortenB22R <49730926+mortensb1@users.noreply.github.com>
2024-04-26 13:56:28 +0200

    Upload puzzle- and piece object

---
+++ Piece.js
@@ -0,0 +1,34 @@
+class Piece
+{
+    constructor(relativeX, relativeY, widthNorm, heightNorm)
+    {
+        this.relativeX = relativeX;
+        this.relativeY = relativeY;
+
+        this.widthNorm = widthNorm;
+        this.heightNorm = heightNorm;
+
+        this.curPosX = 0;
+        this.curPosY = 0;
+
+        this.neighbors = {
+            up: null,
+            down: null,
+            left: null,
+            right: null
+        };
+
+        imagePiece = new
+    }
+
+    draw() 
+    {
+
+    }
+
+    assignPixels(image)
+    {
+        
+    }
+
+}
