b08e4d67cc5720928abe4671007beb5303addc3b
MortenB22R <49730926+mortensb1@users.noreply.github.com>
2024-05-06 14:17:37 +0200

    Peice load image V1

--- sketch.js
+++ sketch.js
@@ -2,19 +2,21 @@ let img, hej;
 
 function preload() {
   img = loadImage("Images\\Cameleon.png");
-  img.loadPixels();
 }
 
 function setup() 
 {
   createCanvas(1920, 1080);
+
+  img.loadPixels();
+
   
-  pice = new Piece(img,0,0,img.width/4,img.height/4);
+  pice = new Piece(img,2,2,img.width/4,img.height/4);
 }
 
 function draw()
 {
   background(220);
   image(img,100,100);
-  image(pice.imagePiece,500,500);
+  image(pice.imagePiece,100 - pice.buffer + pice.relativeX * pice.widthNorm,100 - pice.buffer + pice.relativeY * pice.heightNorm);
 }
