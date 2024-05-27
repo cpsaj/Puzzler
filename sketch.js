c6db29a29e1d812c91e142226d6f0f91a776316c
MortenB22R <49730926+mortensb1@users.noreply.github.com>
2024-05-27 13:18:43 +0200

    Code style

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let img, hej, font;
 
-function preload() {
+function preload() 
+{
   img = loadImage("Images\\Cameleon.png");
   font = loadFont("Fonts\\Fishfingers.ttf");
   sideImgLeft = loadImage("Images\\Side\\Left.png");
@@ -27,8 +28,6 @@ function setup()
 function draw()
 {
   background(220);
-  //image(puzzle.pieces[4][3].imagePiece,100 - puzzle.pieces[0][0].buffer,100 - puzzle.pieces[0][0].buffer);
-  // image(pice.imagePiece,100 - pice.buffer + pice.relativeX * pice.widthNorm,100 - pice.buffer + pice.relativeY * pice.heightNorm);
 
   //Drawing pieces and board
   puzzle.draw();
